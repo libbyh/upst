dbd7a293c2954a32a84ced63316e516e7d0fc013
Libby Hemphill <libbyh@gmail.com>
2014-10-16 14:37:53 -0500

    modified stats.py for Project Gutenberg texts

--- stats.py
+++ stats.py
@@ -5,44 +5,57 @@
 # one line with no blank lines between paragraphs.
 
 # First, just to be sure, we set all the counters to zero:
-lines, blanklines, sentences, words = 0, 0, 0, 0
+lines, paragraphs, sentences, words, chapters, volumes = 0, 0, 0, 0, 0, 0
 
 # Now we need to get a file to work with:
-textfile = open('mdg.txt', 'r')
+textfile = open('pandp.txt', 'r')
 
 # And now we are going to read one line at a time:
 for line in textfile:
     lines += 1
-    # Check for blank lines
-    if line.startswith('\n'):
-        blanklines += 1
+    
+    if line.startswith('CHAPTER'):
+        chapters += 1
+    elif line.startswith('VOLUME') or line.startswith('VOL.'):
+        volumes += 1
+    elif line.startswith('\n'):
+        if prev_line.startswith('\n'):
+            pass
+        else:
+            paragraphs += 1
     else:
         # assume that each sentence ends with . or ! or ?
         # so simply count these characters
         sentences += line.count('.') + line.count('!') + line.count('?')
+        
         # create a list of words
         # use None to split at any whitespace regardless of length
         # so for instance double space counts as one space
-        tempwords = line.split(None)
-        #print tempwords  # test
+        tempwords = line.split(' ')
+        
         # word total count
         words += len(tempwords)
+        
+    prev_line = line
 
 # We'll be tidy and close the file:
 textfile.close()
 
-sentavg = sentences / lines
-wordavg = words / lines
+sentavg = sentences / paragraphs
+wordavg = words / paragraphs
+paraavg = paragraphs / chapters
 
 # And now print the results:
 print '-' * 50
 print "COUNTS "
-print "Paragraphs      : " + str(lines)
-print "Section Breaks: " + str(blanklines)
+print "Lines      : " + str(lines)
+print "Paragraphs : " + str(paragraphs)
+print "Chapters   : " + str(chapters)
+print "Volumes    : " + str(volumes)
 print "Sentences  : " + str(sentences)
 print "Words      : " + str(words)
 print "\n"
 print "AVERAGES"
 print "Sentences per paragraph: " + str(sentavg)
 print "Words per paragraph: " + str(wordavg)
-# print same for section
+print "Paragraphs per chapter: " + str(paraavg)
