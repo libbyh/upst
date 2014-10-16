5a55b9701ef8e86a51260c61abf5eb10a5c1e819
Libby Hemphill <libbyh@gmail.com>
2014-10-16 11:06:11 -0500

    undid what i broke

--- words.py
+++ words.py
@@ -13,8 +13,9 @@ def word_list(textfile):
     textfile.close()
     return word_list
 
-# Create dictionary of word:frequency pairs 
-def word_freq(word_list):
+# Create dictionary of word:frequency pairs
+# by default, sorts dictionary by frequency (desc) 
+def word_freq(word_list, sort="frequency"):
     freq_dic = {}
     
     # Remove punctuation marks:
@@ -30,21 +31,17 @@ def word_freq(word_list):
         except: 
             freq_dic[word] = 1
     
-    return freq_dic
-
-# If you want to sort words by frequency (descending), use this:
-def freq_sorted_dic(freq_dic):
-    # create list of (val, key) tuple pairs
-    freq_dic_sorted = [(val, key) for key, val in freq_dic.items()]
-    # sort by frequency
-    freq_dic_sorted.sort(reverse=True)
-    return freq_dic_sorted
+    # sort the dictionary
+    if sort == "frequency":
+        freq_dic = [(val, key) for key, val in freq_dic.items()]
+        # sort by frequency
+        freq_dic.sort(reverse=True)
+    # if user specificied alphabetical sorting, do that instead
+    elif sort == "alphabetical":
+        freq_dic = freq_dic.items()
+        freq_dic.sort()    
     
-# If you want to sort words by alphabetical order, use this:
-def alpha_sorted_dic(freq_dic):
-    freq_dic_sorted = freq_dic.items()
-    freq_dic_sorted.sort()
-    return freq_dic_sorted
+    return freq_dic
 
 # print frequency dictionaries
 def print_freq(freq_dic):
@@ -52,7 +49,8 @@ def print_freq(freq_dic):
         print word + "," + str(freq)
         
 # Main function
-def main():
+if __name__ == '__main__' :
+    
     # Get file to use from settings.cfg:
     config = SafeConfigParser()
     script_dir = os.path.dirname(__file__)
@@ -66,16 +64,7 @@ def main():
     word_count = len(word_list)
     freq_dic = word_freq(word_list)
     unique_word_count = len(freq_dic)
-    freq_dic_sorted = freq_sorted_dic(freq_dic)
     
     # print the sorted word-frequency pairs
     # use `python words.py > word-freq-pairs.txt' to send output to a text file
-<<<<<<< HEAD
-    print_freq(freq_dic_sorted)
-=======
     print_freq(freq_dic)
-
-if __name__ == '__main__' :
-    main()
-    
->>>>>>> 92c5baf854021e3c786745b5b08529a2f47087ce
