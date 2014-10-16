5527a7086b78bd6ff40285ce2e6f71e7c0e3280c
Libby Hemphill <libbyh@gmail.com>
2014-10-16 11:29:43 -0500

    concordance.py uses config file and command line args

--- concordance.py
+++ concordance.py
@@ -2,22 +2,37 @@
 
 import nltk
 import sys
+import argparse
+from ConfigParser import SafeConfigParser
+import os
+import sys
 
-# First, let's get the word from the user:
+def concordance(textfile, word):
+    textfile = open(textfile)
+    rawtext = textfile.read()
+    tokens = nltk.word_tokenize(rawtext)
+    text = nltk.Text(tokens)
 
-print "Enter the word you would like to see in context:",
-word = raw_input()
+    # Now we can actually look at a word:
+    concordword = text.concordance(word)
 
-# Then we need to transform the base text using a series of steps.
-# Please note this could be one line:
+    return concordword
 
-thefile = open('mdg.txt')
-rawtext = thefile.read()
-tokens = nltk.word_tokenize(rawtext)
-text = nltk.Text(tokens)
+if __name__ == '__main__' :
+    # Get file to use from settings.cfg:
+    config = SafeConfigParser()
+    script_dir = os.path.dirname(__file__)
+    config_file = os.path.join(script_dir, 'settings.cfg')
+    config.read(config_file)
 
-# Now we can actually look at a word:
+    textfile = config.get('files','full_text')
 
-concordword = text.concordance(word)
+    # user must pass a word via command line
+    parser = argparse.ArgumentParser(description="A Python tool for calculating word concordance.")
+    parser.add_argument('-w', '--word', required=True, help="word you want to see in context")
+    args = parser.parse_args()
 
-print(concordword)
+    word = args.word
+
+    concordword = concordance(textfile, word)
+    print(concordword)
