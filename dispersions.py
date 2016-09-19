9b9e218dd8e5e1e75034cd8c4febf02dda21b149
Libby Hemphill <libbyh@gmail.com>
2016-09-19 17:36:58 -0500

    fix dispersions.py for single word

--- dispersions.py
+++ dispersions.py
@@ -1,27 +1,31 @@
 #! /usr/bin/env python
-
 import nltk
-import sys
+import sys, os
+from configparser import SafeConfigParser
 
 # First, let's get input from the user:
-
-print("Enter the word for which you would like to see a dispersion graph:", end=' ')
-word = input()
+word = input("Enter the word for which you would like to see a dispersion graph: ")
 
 # I can't get this to work for multiple words
 # words = raw_input().split(",")
 
 
 # Then we need to transform the base text using a series of steps.
-# Please note this could be one line:
+# Get file to use from settings.cfg:
+config = SafeConfigParser()
+script_dir = os.path.dirname(__file__)
+config_file = os.path.join(script_dir, 'settings.cfg')
+config.read(config_file)
 
-thefile = open('mdg.txt')
+textfile = config.get('files','full_text')
+
+thefile = open(textfile, 'r')
 rawtext = thefile.read()
 tokens = nltk.word_tokenize(rawtext)
 text = nltk.Text(tokens)
 
 # Now we can actually look at a word:
 
-graphed = text.dispersion_plot([words])
+graphed = text.dispersion_plot([word])
 
 # print(similarwords)
