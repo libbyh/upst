4ef1b95fa3a77ac1e3fd931dee4d4610f6b0d3bf
Libby Hemphill <libbyh@gmail.com>
2016-11-17 15:41:55 -0600

    remove religous texts and tfidf from master branch

--- combine-quran.py
+++
@@ -1,8 +0,0 @@
-import glob
-
-read_files = glob.glob("religious-texts/quran-in-english-clearquran-verse-by-verse-txt-edition-allah/*.txt")
-
-with open("religious-texts/Quran.txt", "wb") as outfile:
-    for f in read_files:
-        with open(f, "rb") as infile:
-            outfile.write(infile.read() + b" ")
